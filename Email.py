c42a75525741d6aa216d6222bad638d6cff79b8d
Daniel Heidorn <daniel.j.heidorn@gmail.com>
2017-04-20 17:31:54 -0400

    Added Descriptions

--- Email.py
+++ Email.py
@@ -4,35 +4,50 @@ from email import parser
 import smtplib
 from time import sleep
 import sys
-    
+
 #Definitions
+
+#Loading Bar
+def loading(z):
+    print "Connecting",
+    for i in range(z):
+        sleep(.1)
+        print ". ",
+"""
+Need to update to IMAP
+"""
 def read():
     try:
-        pop_conn = poplib.POP3_SSL('pop.gmail.com')
-        pop_conn.user(user)
-        pop_conn.pass_(pwd)
-        messages = [pop_conn.retr(i) for i in range(1, 11)]
+        con = poplib.POP3_SSL('pop.gmail.com')
+        con.user(user)
+        con.pass_(pwd)
+        messages = [con.retr(i) for i in range(1, 11)]
         messages = ["\n".join(mssg[1]) for mssg in messages]
         messages = [parser.Parser().parsestr(mssg) for mssg in messages]
         for message in messages:
             print message['subject']
             #print message.get_payload()
-        pop_conn.quit()
+        con.quit()
     except:
         print("Failed to recieve mail! Check connection, password, and username!")
-        
+
+#Sending e-mail to others                
 def email():
     recipient = str(raw_input("To: "))
     subject = str(raw_input("Subject: "))
     body = str(raw_input("Body: "))
     FROM = user
     
-        #Shout out to David Akwii on stackexchange for code on line  - 
+    """Shout out to David Akwii on stackexchange 
+    for code on line  49-51 for help formating"""
+     
+    
     TO = recipient if type(recipient) is list else [recipient]
     SUBJECT = subject
     TEXT = body
     
-        # Prepare actual message
+    #Format for sending
+    
     message = """From: %s\nTo: %s\nSubject: %s\n\n%s
     """ % (FROM, ", ".join(TO), SUBJECT, TEXT)
     print message
@@ -46,15 +61,11 @@ def email():
         print("Mail sent!")
     except:
         print("Failed to send mail! Check connection, password, and username!")
-        
-        
+          
 #Main Start
 user = str(raw_input("G-mail username (make sure to use @gmail.com): "))
 pwd = str(raw_input("G-mail password: "))
-print "Connecting",
-for i in range(10):
-    sleep(.1)
-    print ". ",
+loading(5)
 print("")    
 gmail_user = user
 gmail_pwd = pwd
@@ -64,11 +75,13 @@ while True:
     if command == "/email":
         email() #If /email typed then it will activate email send script
     elif command == "/help":
-        print("/email for email, /read for reading last 10 emails, and /exit to exit") #If /help typed it shows list
+        print("/email for email, /read for reading last 10 emails, and /exit to exit") 
+        #If /help typed it shows list
     elif command == "/read":
         read()
     elif command == "/exit":
         sys.exit()
     else:
-        print("Please enter a valid command") #In case of something written wrong it returns and gives error code
+        print("Please enter a valid command") 
+        #In case of something written wrong it returns and gives error code
 
